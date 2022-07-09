5490e95cb05f825b75b4353f3cdee90515a56131
nik.m <nik.m@investment.com.au>
2022-07-09 15:57:11 +0300

    Update Email component with new data

--- api/routes/emails.js
+++ api/routes/emails.js
@@ -1,9 +1,12 @@
 let express = require('express');
 let router = express.Router();
 
-/* GET users listing. */
+// JSON keeps all the emails
+const STORAGE_ENDPOINT = "../storage/email.json";
+
+/* GET emails listing. */
 router.get('/', function(req, res, next) {
-    const items = require("../public/files/emails/small.json"); // local data storage
+    const items = require(STORAGE_ENDPOINT); // local data storage
     res.status(200).json(items);
 });
 
--- src/components/Email.js
+++ src/components/Email.js
@@ -1,17 +1,54 @@
-import {SimpleCell} from "@vkontakte/vkui";
+import {Avatar, SimpleCell} from "@vkontakte/vkui";
 import React from "react";
 import "./Email.scss";
 
+import { Icon12Circle, Icon12CircleOutline, Icon16BookmarkOutline, Icon16Bookmark } from '@vkontakte/icons';
+
 const SHORT_BODY_LENGTH = 50;
 
-export default function Email({ title, author, text, dateTime }) {
+export default function Email({ title, author, text, dateTime, read, flag }) {
     const shortBody = text.substring(0, SHORT_BODY_LENGTH);
+
     return (
         <SimpleCell className="email-item">
-            <span className="email__sender">{author.name}</span>
-            <span className="email__subject">{title}</span>
-            <span className="email__body">{shortBody}</span>
-            <span className="email__date">{dateTime}</span>
+            <div className="email-mark-as-read__container">
+                {
+                    read ? (
+                        <Icon12CircleOutline className="mark-as-read__item" title="Пометить непрочитанным"/>
+                    ) : (
+                        <Icon12Circle className="mark-as-read__item" title="Пометить прочитанным"/>
+                    )
+                }
+            </div>
+
+            <Avatar className="email-avatar" src={author.avatar} />
+
+            <div className="email-sender__container">
+                <span className="sender__item" title={`${author.name} ${author.email}`}>{author.name}</span>
+            </div>
+
+            <div className="email-flag__container">
+                {
+                    flag ? (
+                        <Icon16Bookmark title="Снять флажок"/>
+                    ) : (
+                        <Icon16BookmarkOutline title="Пометить флажком"/>
+                    )
+                }
+            </div>
+
+            <div className="email-subject__container">
+                <span className="email__subject">{title}</span>
+            </div>
+
+            <div className="email-body__container">
+                <span className="email__body">{shortBody}</span>
+            </div>
+
+            <div className="email-date__container">
+                <div title={dateTime} className="email__date">{dateTime}</div>
+            </div>
         </SimpleCell>
     );
 }
+
